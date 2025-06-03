96db8033e618b92b53f42234464e03919a1b2a88
Pedro Nakamura <nakanzaDev@gmail.com>
2025-06-03 11:44:36 -0300

    Inventory Slot Display

---
+++ Assets/_Code/Script/UI/Inventory/InventorySlotDisplay.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NGPTask.Item;
+
+namespace NGPTask.UI {
+    public class InventorySlotDisplay : MonoBehaviour {
+
+        public ItemType ItemTypeCurrent { get; private set; }
+
+        [Header("References")]
+
+        [SerializeField] private Image _iconDisplay;
+        [SerializeField] private TextMeshProUGUI _amountDisplay;
+
+        public void DisplayUpdate(Inventory.InventorySlot slotDisplayed) {
+            ItemTypeCurrent = slotDisplayed.ItemType;
+            _iconDisplay.sprite = ItemTypeCurrent.Icon;
+            _amountDisplay.text = $"{slotDisplayed.ItemAmount}";
+        }
+
+    }
+}
