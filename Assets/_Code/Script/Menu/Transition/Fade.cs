73a692616be204c04732bcdaab81932856adb08b
Pedro Nakamura <nakanzaDev@gmail.com>
2025-06-02 22:09:03 -0300

    Menu Transitions

---
+++ Assets/_Code/Script/Menu/Transition/Fade.cs
@@ -0,0 +1,10 @@
+
+namespace NGPTask.UI {
+    public class Fade : Menu {
+
+        protected override void TransitionBehaviour(float currentPhase) {
+            _canvasGroup.alpha = _transitionCurve.Evaluate(_isOpening ? currentPhase : (1f - currentPhase));
+        }
+
+    }
+}
---
+++ Assets/_Code/Script/Menu/Transition/Slide.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace NGPTask.UI {
+    public class Slide : Menu {
+
+        [Header("Slide")]
+
+        [SerializeField] private Vector2 _openStartPos;
+        [SerializeField] private Vector2 _openEndPos;
+        [SerializeField] private Vector2 _closeStartPos;
+        [SerializeField] private Vector2 _closeEndPos;
+
+        [Header("Cache")]
+
+        private RectTransform _rectTransform;
+
+        protected override void Awake() {
+            base.Awake();
+
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        protected override void TransitionBehaviour(float currentPhase) {
+            _rectTransform.anchoredPosition = Vector2.Lerp(_isOpening ? _openStartPos : _openEndPos, _isOpening ? _closeStartPos : _closeEndPos, _transitionCurve.Evaluate(currentPhase));
+        }
+
+    }
+}
