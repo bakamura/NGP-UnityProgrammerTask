b099f22b138fe4557416a5684d31e8bb0d1af576
Pedro Nakamura <nakanzaDev@gmail.com>
2025-06-03 14:27:03 -0300

    Fix Swap changing during drag

--- Assets/_Code/Script/UI/Inventory/InventorySlotDisplay.cs
+++ Assets/_Code/Script/UI/Inventory/InventorySlotDisplay.cs
@@ -1,11 +1,12 @@
-using NGPTask.Item;
-using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using TMPro;
+using NGPTask.Item;
 
 namespace NGPTask.UI {
+    [RequireComponent (typeof(Button))]
     public class InventorySlotDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
         public ItemType ItemTypeCurrent { get; private set; }
@@ -16,6 +17,21 @@ namespace NGPTask.UI {
         [SerializeField] private TextMeshProUGUI _amountDisplay;
         private int _index;
 
+        private void Awake() {
+            if (TryGetComponent(out Button btn)) btn.onClick.AddListener(TryUse);
+            else Debug.LogError($"Couldn't get {nameof(Button)} attached to {nameof(InventorySlotDisplay)} '{name}'. Use won't work properly");
+        }
+
+        public void OnPointerEnter(PointerEventData eventData) {
+            if(ItemTypeCurrent != null && !InventoryDragDisplay.Instance.gameObject.activeInHierarchy) InventoryInfoDisplay.Instance.InfoDisplay(ItemTypeCurrent.DisplayName, ItemTypeCurrent.Description);
+            InventoryInfoDisplay.Instance.indexCurrent = _index;
+        }
+
+        public void OnPointerExit(PointerEventData eventData) {
+            InventoryInfoDisplay.Instance.HideDisplay();
+            if (ItemTypeCurrent != null && !InventoryDragDisplay.Instance.gameObject.activeInHierarchy && Mouse.current.leftButton.isPressed) InventoryDragDisplay.Instance.DragDisplay(_iconDisplay.sprite, _index);
+        }
+
         public void DisplayUpdate(Inventory.InventorySlot slotDisplayed, int index) {
             ItemTypeCurrent = slotDisplayed.ItemType;
             _iconDisplay.sprite = ItemTypeCurrent?.Icon;
@@ -24,14 +40,11 @@ namespace NGPTask.UI {
             _index = index;
         }
 
-        public void OnPointerEnter(PointerEventData eventData) {
-            if(ItemTypeCurrent != null) InventoryInfoDisplay.Instance.InfoDisplay(ItemTypeCurrent.DisplayName, ItemTypeCurrent.Description);
-            InventoryInfoDisplay.Instance.indexCurrent = _index;
-        }
-
-        public void OnPointerExit(PointerEventData eventData) {
-            InventoryInfoDisplay.Instance.HideDisplay();
-            if (ItemTypeCurrent != null && Mouse.current.leftButton.isPressed) InventoryDragDisplay.Instance.DragDisplay(_iconDisplay.sprite, _index);
+        private void TryUse() {
+            if (ItemTypeCurrent != null && ItemTypeCurrent is IUsable) {
+                IUsable usableItem = ItemTypeCurrent as IUsable;
+                if(usableItem.CanUse) usableItem.Use();
+            }
         }
 
     }
